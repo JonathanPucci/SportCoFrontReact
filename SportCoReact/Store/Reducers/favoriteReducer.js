60603f8b0c17db472c8de213e130b22007783240
JonathanPucci <jonathanpucci06@gmail.com>
2020-03-22 11:31:07 +0100

    added Store

---
+++ SportCoReact/Store/Reducers/favoriteReducer.js
@@ -0,0 +1,30 @@
+// Store/Reducers/favoriteReducer.js
+
+const initialState = { favoriteSportss: [] }
+
+function toggleFavorite(state = initialState, action) {
+    let nextState
+    switch (action.type) {
+        case 'TOGGLE_FAVORITE':
+            const favoriteSportIndex = state.favoriteSports.findIndex(item => item.id === action.value.id)
+            if (favoriteSportIndex !== -1) {
+                // Le Sport est déjà dans les favoris, on le supprime de la liste
+                nextState = {
+                    ...state,
+                    favoriteSports: state.favoriteSports.filter((item, index) => index !== favoriteSportIndex)
+                }
+            }
+            else {
+                // Le Sport n'est pas dans les Sports favoris, on l'ajoute à la liste
+                nextState = {
+                    ...state,
+                    favoriteSports: [...state.favoriteSports, action.value]
+                }
+            }
+            return nextState || state
+        default:
+            return state
+    }
+}
+
+export default toggleFavorite
